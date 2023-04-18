8a80a9509b6e2cec1db971c6380bd8f989407bf3
Norbert Csipak <norbertcipak@me.com>
2023-04-18 12:10:42 +0100

    5 flats showing as index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "flats", to: "flats#index"
+  root to: 'flats#index'
+  resources :flats
 end
